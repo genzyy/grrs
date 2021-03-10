facda56b2035666a7170ef4fe17b55d898037b4b
Rishit Pandey <rishpandey8097@gmail.com>
2021-03-10 16:34:14 +0530

    Greetings

---
+++ README.md
@@ -0,0 +1,18 @@
+<div align="center">
+    <h1>grrs - Greetings in CLI from Rust!</h1>
+</div>
+
+### Dependencies Used
+
+- structopt = "0.2.14"
+
+### About structopt
+
+- Read the official docs [here](https://docs.rs/structopt/0.3.21/structopt/)
+
+### Usage
+
+```bash
+    ./target/release/grrs -g
+    Hello, from Rust!
+```
---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "demo")]
+
+struct Opt {
+    #[structopt(short = "g", long = "greet")]
+    pub greet: bool,
+}
+
+fn main() {
+    let opt = Opt::from_args();
+    if opt.greet {
+        println!("Hello, from Rust!");
+    } else {
+        println!("You missed something...");
+    }
+}
