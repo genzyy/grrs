514e9c015a81b190d8314641615234eff95033ef
Rishit Pandey <rishpandey8097@gmail.com>
2021-03-10 16:46:24 +0530

    documentation

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
+// Crate used for arguments structure.
 use structopt::StructOpt;
 
+// Activating Debug Mode
 #[derive(StructOpt, Debug)]
-#[structopt(name = "demo")]
+// About the app
+#[structopt(name = "demo", about = "Greetings from CLI using Rust!")]
 
 struct Opt {
+    // Defining cli arguments flag as short and long ones.
     #[structopt(short = "g", long = "greet")]
+    // greet vairable defined as public so that there is no pre defined scope of greet variable
     pub greet: bool,
 }
 
+// main function here
 fn main() {
+    // namespace for arguments
     let opt = Opt::from_args();
     if opt.greet {
         println!("Hello, from Rust!");
